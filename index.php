6506e7551938fd6af610ff694bba13068706851d
jmyungjoon <jmyungjoon@gmail.com>
2020-05-03 03:23:16 +0800

    animation 2

--- index.php
+++ index.php
@@ -11,9 +11,15 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
     <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <link href="aos.css" rel="stylesheet">
     <link href="style.css?id=1234" rel="stylesheet">
-    
+    <!-- <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+    <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
+
     <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
@@ -35,16 +41,17 @@
   </head>
   <body class="text-center">
 <!-- <body class="text-center"> -->
-  <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <div class="cover-container d-flex w-100w-100 h-100 p-3 mx-auto flex-column">
       <header class="masthead mb-auto">
         <div class="inner">
-          <h3 class="masthead-brand"><a href="http://ittcserver.com">ITTC</a></h3>
+          
+          <h4 class="masthead-brand" data-aos="fade-down"><a href="http://ittcserver.com">ITTC</a></h4>
           <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="http://ittcserver.net">Home</a>
-            <a class="nav-link" href="#">Features</a>
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link active" href="http://ittcserver.net" data-aos="fade-down">Home</a>
+            <a class="nav-link" href="#" data-aos="fade-down">Features</a>
+            <a class="nav-link" href="#" data-aos="fade-down">Contact</a>
                 <div class="login-btn-wrapper" >
-                    <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal" class="btn navbar-btn btn-sign-in login-btn-in navbar-right" style="background-color : darkred; margin-left: 10px; padding-top: 3px; color: white; padding: 3px; margin-left: 10px;">Sign Up</button>
+                    <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal" class="btn navbar-btn btn-sign-in login-btn-in navbar-right" style="background-color : darkred;padding-top: 4px;padding-bottom: 4px;margin-bottom: 5px;margin-top: 1px;padding-right: 10px;padding-left: 10px;border-right-width: 10px;" data-aos="fade-down">Sign Up</button>
                 </div>
           </nav>
         </div>
@@ -52,22 +59,39 @@
       <div class="container text-center" >
     
         <main role="main" class="inner cover">
-          <h1 class="cover-heading" >Web Developer David</h1>
-            <div class="text-center">
-              <img src="david.jpg" class = "rounded" style="width: 300px;">
-              <p class="lead">Welcome to my developer site! <br> You will meet some interesting works of web application or home pages that I am making as studying projects. <br>Please bear with me. Many parts are still under development.</p>
+            <div data-aos="fade-down">
+                <h2 class="cover-heading" >Web Developer David</h2>
             </div>
-            <p class="lead">
-            <a href="http://ittcserver.net/webapplication" class="btn btn-lg btn-secondary">Web Application</a>
-            </p>
+            <div class="text-center">
+                    <img src="david.jpg" class="img-fluid wow tada z-depth-1 animated rounded"  data-wow-delay="0.2s" style="visibility: visible; animation-name: tada; animation-iteration-count: 1; animation-delay: 2s; width: 300px;" >
+                    <p class="lead" data-aos="fade-up">Welcome to my developer site! <br> You will meet some interesting works of web application or home pages that I am making as studying projects. <br>Please bear with me. Many parts are still under development.</p>
+                      <a href="http://ittcserver.net/webapplication" class="btn btn-lg btn-primary" data-aos="fade-up" data-aos-delay="1000">Web Application</a>
+            </div>
+            <!-- <p class="lead"> -->
+                
+            <!-- </p> -->
         </main>
       </div>
+      
     <footer class="mastfoot mt-auto">
       <div class="inner">
         <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
       </div>
     </footer>
   </div>
+  <!-- Vendor JS Files -->
+  <!-- <script src="assets/vendor/jquery/jquery.min.js"></script> -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/js/mdb.min.js"></script>    
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
   </body>
 </html>
 
