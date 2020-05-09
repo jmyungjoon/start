9682b0f0bbc40aefd1ca8555ed5227d70622e8b5
jmyungjoon <jmyungjoon@gmail.com>
2020-05-10 01:30:47 +0800

    OOP 2

--- index.php
+++ index.php
@@ -6,82 +6,54 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v3.8.6">
-    <title>ITTC Server Start Page</title>
+    <title>Web Application</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>  
-    <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <!-- <link href="style.css?id=1234" rel="stylesheet"> -->
-    <!-- <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> -->
     <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
     <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
     <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
     <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
     <link href="assets/vendor/aos/aos.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.16.0/css/mdb.min.css" rel="stylesheet">
-
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 1024px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
     <link href="cover.css" rel="stylesheet">
   </head>
+  
   <body class="text-center">
-<!-- <body class="text-center"> -->
-  <div class="cover-container d-flex w-100w-100 h-100 p-3 mx-auto flex-column">
-      <header class="masthead mb-auto">
-        <div class="inner">
-          
-          <h4 class="masthead-brand" data-aos="fade-down"><a href="http://ittcserver.com">ITTC</a></h4>
-          <nav class="nav nav-masthead justify-content-center">
-            <a class="nav-link active" href="http://ittcserver.net" data-aos="fade-down">Home</a>
-            <a class="nav-link" href="#" data-aos="fade-down">Features</a>
-            <a class="nav-link" href="#" data-aos="fade-down">Contact</a>
-                <div class="login-btn-wrapper" >
-                    <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal" class="btn navbar-btn btn-sign-in login-btn-in navbar-right" style="background-color : darkred;padding-top: 4px;padding-bottom: 4px;margin-bottom: 5px;margin-top: 1px;padding-right: 10px;padding-left: 10px;border-right-width: 10px;" data-aos="fade-down">Sign Up</button>
-                </div>
-          </nav>
+    <div class="cover-container d-flex w-100w-100 h-100 p-3 mx-auto flex-column">
+        <header class="masthead mb-auto">
+          <div class="inner">
+            <h4 class="masthead-brand" data-aos="fade-down"><a href="http://ittcserver.com">ITTC</a></h4>
+            <nav class="nav nav-masthead justify-content-center">
+              <a class="nav-link active" href="http://ittcserver.net" data-aos="fade-down">Home</a>
+              <a class="nav-link" href="#" data-aos="fade-down">Features</a>
+              <a class="nav-link" href="#" data-aos="fade-down">Contact</a>
+              <div class="login-btn-wrapper" >
+                  <button type="button" name="age" id="age" data-toggle="modal" data-target="#add_data_Modal" class="btn navbar-btn btn-sign-in login-btn-in navbar-right" style="background-color : darkred;padding-top: 4px;padding-bottom: 4px;margin-bottom: 5px;margin-top: 1px;padding-right: 10px;padding-left: 10px;border-right-width: 10px;" data-aos="fade-down">Sign Up</button>
+              </div>
+            </nav>
+          </div>
+        </header>
+        <div class="container text-center" >
+          <main role="main" class="inner cover">
+              <div data-aos="fade-down">
+                  <h2 class="cover-heading" >Web Developer David</h2>
+              </div>
+              <div class="text-center">
+                  <img src="david.jpg" class="img-fluid wow tada z-depth-1 animated rounded"  data-wow-delay="0.2s" style="visibility: visible; animation-name: tada; animation-iteration-count: 1; animation-delay: 2s; width: 300px;" >
+              </div>
+              <a href="../webapplication" class="btn btn-md btn-primary" data-aos="fade-up" data-aos-delay="1000" style="margin-top: 15px">Web Application</a>
+          </main>
         </div>
-      </header>
-      <div class="container text-center" >
-    
-        <main role="main" class="inner cover">
-            <div data-aos="fade-down">
-                <h2 class="cover-heading" >Web Developer David</h2>
-            </div>
-            <div class="text-center">
-                    <img src="david.jpg" class="img-fluid wow tada z-depth-1 animated rounded"  data-wow-delay="0.2s" style="visibility: visible; animation-name: tada; animation-iteration-count: 1; animation-delay: 2s; width: 300px;" >
-                    <!-- <p class="lead" data-aos="fade-up">Welcome to my developer site! <br> You will meet some interesting works of web application or home pages that I am making as studying projects. <br>Please bear with me. Many parts are still under development.</p> -->
-            </div>
-            
-            <a href="http://ittcserver.net/webapplication" class="btn btn-md btn-primary" data-aos="fade-up" data-aos-delay="1000" style="margin-top: 15px">Web Application</a>
-            <!-- <p class="lead"> -->
-                
-            <!-- </p> -->
-        </main>
-      </div>
-      
-    <footer class="mastfoot mt-auto">
-      <div class="inner">
-        <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      </div>
-    </footer>
-  </div>
+        
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Cover template for <a href="https://getbootstrap.com/">Bootstrap</a>, by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+        </div>
+      </footer>
+    </div>
+  
   <!-- Vendor JS Files -->
-  <!-- <script src="assets/vendor/jquery/jquery.min.js"></script> -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
@@ -140,9 +112,6 @@
             success:function(data){
                 setTimeout(function() {$("#user-availability-status").html(data);
                 $("#loaderIcon").hide();}, 1000);
-                // $("#user-availability-status").hide();}, 10000);
-                // $("#user-availability-status").html(data);
-                // $("#loaderIcon").hide();
             },
             error:function (){}
             });
